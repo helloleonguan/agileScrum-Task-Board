c96124b85b452b14485d18af3a9cb0d46eceab7d
u5684206 <u5684206@anu.edu.au>
2015-12-02 23:31:34 +0800

    Update Enum class Status

--- src/Status.java
+++ src/Status.java
@@ -2,4 +2,14 @@
  * Created by Administrator on 2015/12/2/0002.
  */
 public enum Status {
+    TO_DO("To Do"),
+    IN_PROGRESS("In Progress"),
+    TO_VERIFY("To Verify"),
+    DONE("Done");
+
+    private String status; // For printing Status.DONE.status. 
+
+    Status(String s) {
+        this.status = s;
+    }
 }
